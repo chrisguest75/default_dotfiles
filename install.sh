4f558b1dca3cf08669d54ce721c6cc7e23f68c29
Chris Guest <chris.guest@condenastint.com>
2020-01-21 10:13:57 +0000

    Improve install script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
+# Is there a backup of config
 if [[ ! -f ~/.zshrc_original ]]; then 
-    mv ~/.zshrc ~/.zshrc_original
+    # Does zshrc exist?
+    if [[ -f ~/.zshrc ]]; then 
+        # Is zshrc already a symlink
+        if [[ ! readlink ~/.zshrc ]]; then 
+            mv ~/.zshrc ~/.zshrc_original
+        else
+            echo "~/.zshrc is already a symlink"
+        fi
+    else
+        echo "~/.zshrc does not exist"
+        # Create symlink to profile
+        ln -s /home/vagrant/Code/default_dotfiles/.zshrc ~/.zshrc
+    fi
 else
     echo "~/.zshrc_original backup file located.  Please rename and try again"
 fi
@@ -10,7 +23,5 @@ fi
 # Copy over my theme
 cp chrisguest.zsh-theme $ZSH/themes
 
-# Create symlink to profile
-ln -s /home/vagrant/Code/default_dotfiles/.zshrc ~/.zshrc
 
 
