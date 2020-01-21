238d6537bc43315569d12429233d66519bd4913a
Chris Guest <chris.guest@condenastint.com>
2020-01-21 10:28:47 +0000

    Fix the install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ ! -f ~/.zshrc_original ]]; then
     # Does zshrc exist?
     if [[ -f ~/.zshrc ]]; then 
         # Is zshrc already a symlink
-        if [[ ! readlink ~/.zshrc ]]; then 
+        if ! readlink ~/.zshrc; then 
             mv ~/.zshrc ~/.zshrc_original
         else
             echo "~/.zshrc is already a symlink"
