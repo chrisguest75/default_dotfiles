78555306bbc8cd5a30d36bad0db55af9d4986448
Chris Guest <chris.guest@condenastint.com>
2020-01-21 10:05:36 +0000

    Setup my theme

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+set -euf -o pipefail
 
-mv ~/.zshrc ~/.zshrc_original
+if [[ ! -f ~/.zshrc_original ]]; then 
+    mv ~/.zshrc ~/.zshrc_original
+else
+    echo "~/.zshrc_original backup file located.  Please rename and try again"
+fi
 
+# Copy over my theme
+cp chrisguest.zsh-theme $ZSH/themes
+
+# Create symlink to profile
 ln -s /home/vagrant/Code/default_dotfiles/.zshrc ~/.zshrc
+
+
