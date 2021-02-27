91f96d3b6889f9735a09e9e6652b603d4c0b3440
Chris Guest <chris.guest@condenastint.com>
2021-02-27 11:20:16 +0000

    Install pyenv

--- machines/maccni-00213/maccni-00213.sh
+++ machines/maccni-00213/maccni-00213.sh
@@ -9,3 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 source /usr/local/share/zsh/site-functions/aws_zsh_completer.sh
+
+eval "$(pyenv init -)"
