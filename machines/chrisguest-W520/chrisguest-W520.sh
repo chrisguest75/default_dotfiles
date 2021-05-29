23d5f2713c24e23085e4e9b89ecdd8d6dc7db981
Chris Guest <chris.guest75@hotmail.com>
2021-05-29 12:05:25 +0100

    Add krew and brew

--- machines/chrisguest-W520/chrisguest-W520.sh
+++ machines/chrisguest-W520/chrisguest-W520.sh
@@ -12,3 +12,7 @@ eval "$(pyenv init -)"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
