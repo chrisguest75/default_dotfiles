b8991daed31fbf2638125911535cd5e912949b2f
Chris Guest <chris.guest75@hotmail.com>
2021-08-15 11:33:56 +0100

    Some example defaults for macosx.

---
+++ machines/chrisguest-MacBook-Pro/defaults.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+defaults write NSGlobalDomain com.apple.trackpad.scaling 2
+#defaults write NSGlobalDomain com.apple.mouse.scaling 2
+
+defaults write NSGlobalDomain com.apple.sound.beep.flash -bool false
+defaults write NSGlobalDomain com.apple.springing.delay -int 0.5
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool true
+defaults write NSGlobalDomain com.apple.trackpad.scaling -int 2
+
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+#"Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
